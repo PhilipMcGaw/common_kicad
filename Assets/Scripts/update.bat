b5a299d4c57fcfc7165dfa1cf8911e0f1e94bbe3
Skippy <philip@mcgaw.eu>
2025-08-14 22:34:10 +0100

    added scripts folder

---
+++ Assets/Scripts/update.bat
@@ -0,0 +1,18 @@
+@echo off
+
+echo Purges old copyies if they are here
+del main.zip /F
+rmdir common_kicad-main /Q /S
+
+
+echo grab and un-zip a new copy
+
+bin\wget.exe https://github.com/PhilipMcGaw/common_kicad/archive/refs/heads/main.zip
+tar -xf main.zip
+
+echo place the new files in the right location
+xcopy common_kicad-main\Assets\* ..\ /s /e /h /y
+
+echo purge the downloads
+del main.zip /F
+rmdir common_kicad-main /Q /S
